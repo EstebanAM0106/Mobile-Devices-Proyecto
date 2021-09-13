72a48be28b4a4616ad40992db6ad017066e116d6
Uber8bits <90074656+Uber8bits@users.noreply.github.com>
2021-09-12 21:42:40 -0500

    Add files via upload

---
+++ PLantas.sql
@@ -0,0 +1,35 @@
+CREATE DATABASE Plantas;
+
+CREATE TABLE Planta
+(
+  ID_planta INT NOT NULL,
+  Altura INT NOT NULL,
+  Diametro INT NOT NULL,
+  Descripcion VARCHAR(300) NOT NULL,
+  URL_imagen VARCHAR(150) NOT NULL,
+  PRIMARY KEY (ID_planta)
+);
+
+CREATE TABLE Taxonomia
+(
+  Reino VARCHAR(20) NOT NULL,
+  Division VARCHAR(30) NOT NULL,
+  Clase VARCHAR(30) NOT NULL,
+  Orden VARCHAR(30) NOT NULL,
+  Familia VARCHAR(20) NOT NULL,
+  Genero VARCHAR(20) NOT NULL,
+  Especie VARCHAR(30) NOT NULL,
+  ID_planta INT NOT NULL,
+  FOREIGN KEY (ID_planta) REFERENCES Planta(ID_planta)
+);
+
+CREATE TABLE Recomendaciones
+(
+  Ciclo INT NOT NULL,
+  Unidad_ciclo VARCHAR(10) NOT NULL,
+  Maceta BIT NOT NULL,
+  Luminosidad INT NOT NULL,
+  Otras_ VARCHAR(300) NOT NULL,
+  ID_planta INT NOT NULL,
+  FOREIGN KEY (ID_planta) REFERENCES Planta(ID_planta)
+);
